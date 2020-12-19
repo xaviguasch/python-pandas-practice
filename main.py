3f667bd75e008d93636e44b8462e0d32a53eca2f
xaviguasch <xaviguasch@gmail.com>
2020-12-19 14:50:29 +0100

    added python pandas practice

---
+++ main.py
@@ -0,0 +1,73 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
+
+
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# # print(data["temp"])
+#
+# data_dict = data.to_dict()
+# # print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+
+
+
+# average = sum(temp_list) / len(temp_list)
+# print(average)
+
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# Get Data in columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get Data in rows
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_f = monday_temp * 9/5 + 32
+# print(monday_temp_f)
+
+# Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+
+
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
